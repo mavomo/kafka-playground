8049a84a1234f0777ffdc50086c150203d3a608c
Michelle AVOMO <michelle.avomo.ext@loccitane.com>
2024-10-22 11:19:14 +0200

    feature definition

--- src/test/java/com/playground/kafkaplayground/KafkaPlaygroundApplicationTests.java
+++ src/test/java/com/playground/kafkaplayground/KafkaPlaygroundApplicationTests.java
@@ -1,9 +1,11 @@
 package com.playground.kafkaplayground;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Disabled
 class KafkaPlaygroundApplicationTests {
 
     @Test
