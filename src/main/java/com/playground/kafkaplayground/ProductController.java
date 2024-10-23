2f56a000c6e49e6fb59507fcf2e6dbd5ddc29b50
Michelle AVOMO <michelle.avomo.ext@loccitane.com>
2024-10-23 11:04:00 +0200

    adding dockerfile - removing war nature

--- src/main/java/com/playground/kafkaplayground/ProductController.java
+++ src/main/java/com/playground/kafkaplayground/ProductController.java
@@ -1,5 +1,6 @@
 package com.playground.kafkaplayground;
 
+import com.playground.kafkaplayground.domain.Product;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
--- src/test/java/com/playground/kafkaplayground/ProductControllerIT.java
+++ src/test/java/com/playground/kafkaplayground/ProductControllerIT.java
@@ -2,8 +2,7 @@ package com.playground.kafkaplayground;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.type.CollectionType;
-import org.assertj.core.api.Assert;
-import org.assertj.core.api.Assertions;
+import com.playground.kafkaplayground.domain.Product;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -15,7 +14,6 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
