42a5d7d1686fb82969e5534ae6720040cc560d4a
mickael.boixiere <boixiere.mickael@gmail.com>
2024-10-23 10:52:39 +0200

    feat: add hardcoded products list reachable from an endpoint

--- src/test/java/com/playground/kafkaplayground/KafkaPlaygroundApplicationTests.java
+++ src/test/java/com/playground/kafkaplayground/KafkaPlaygroundApplicationTests.java
@@ -1,11 +1,9 @@
 package com.playground.kafkaplayground;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-@Disabled
 class KafkaPlaygroundApplicationTests {
 
     @Test
