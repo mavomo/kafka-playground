2a5e8f433b7e4a06bebc974197c854fb4f42e038
mickael.boixiere <boixiere.mickael@gmail.com>
2024-10-29 12:04:14 +0100

    feat(inventory): add inventory layer

--- src/main/java/com/playground/kafkaplayground/domain/Inventory.java
+++ src/main/java/com/playground/kafkaplayground/domain/Inventory.java
@@ -3,7 +3,6 @@ package com.playground.kafkaplayground.domain;
 import java.time.LocalDateTime;
 
 public record Inventory(
-        String Id,
         LocalDateTime createdAt,
         Long productId,
         Long productQuantity
--- src/main/java/com/playground/kafkaplayground/infra/InventoryService.java
+++ src/main/java/com/playground/kafkaplayground/infra/InventoryService.java
@@ -1,27 +1,41 @@
 package com.playground.kafkaplayground.infra;
 
+import com.playground.kafkaplayground.ProductService;
 import com.playground.kafkaplayground.domain.Inventory;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
-import java.util.UUID;
 
 @Service
 public class InventoryService {
 
-    private final KafkaTemplate<String, Inventory> kafkaTemplate;
+    private static final String TOPIC_NAME = "dev.playground.inventory.created";
+    private static final Long DEFAULT_PRODUCT_QUANTITY = 10L;
 
-    public InventoryService(KafkaTemplate<String, Inventory> kafkaTemplate) {
+    private final KafkaTemplate<String, Inventory> kafkaTemplate;
+    private final ProductService productService;
+
+    public InventoryService(
+            KafkaTemplate<String, Inventory> kafkaTemplate,
+            ProductService productService
+    ) {
         this.kafkaTemplate = kafkaTemplate;
+        this.productService = productService;
     }
 
-    public void sendMessage(String topic, String key, String message) {
-        kafkaTemplate.send(topic, key, new Inventory(
-                UUID.randomUUID().toString(),
-                LocalDateTime.now(),
-                1L,
-                1L
-        ));
+    public void initialize() {
+        productService.products().forEach(product -> {
+            var record = new ProducerRecord<>(
+                    TOPIC_NAME,
+                    product.id().toString(),
+                    new Inventory(
+                            LocalDateTime.now(),
+                            product.id(),
+                            DEFAULT_PRODUCT_QUANTITY
+                    ));
+            kafkaTemplate.send(record);
+        });
     }
 }
